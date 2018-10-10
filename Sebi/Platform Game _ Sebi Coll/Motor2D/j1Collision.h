5d2edf2b817b9982406d1fca9087485859384724
Sebi Lopez <sebi8818@gmail.com>
2018-10-11 01:59:01 +0200

    Added Parallel Collision code

---
+++ Sebi/Platform Game _ Sebi Coll/Motor2D/j1Collision.h
@@ -0,0 +1,78 @@
+#ifndef __ModuleCollision_H__
+#define __ModuleCollision_H__
+
+#define MAX_COLLIDERS 500
+
+#include "j1Module.h"
+#include "SDL/include/SDL_rect.h"
+
+enum COLLIDER_TYPE
+{
+	COLLIDER_NONE = -1,
+
+	COLLIDER_WALL,
+	COLLIDER_PLAYER,
+	COLLIDER_ENEMY,
+	COLLIDER_PLAYER_SHOT,
+	COLLIDER_PLAYER2_SHOT,
+	COLLIDER_ENEMY_SHOT,
+	COLLIDER_CAR,
+	COLLIDER_POWERUP_L,
+	COLLIDER_POWERUP_S,
+	COLLIDER_BALL,
+	COLLIDER_BALL2,
+	COLLIDER_GOD,
+	COLLIDER_POWERUP_G,
+	COLLIDER_POWERUP_H,
+	COLLIDER_PLAYER1_EXPLOSION,
+	COLLIDER_PLAYER2_EXPLOSION,
+
+
+	COLLIDER_MAX
+};
+
+struct Collider
+{
+	SDL_Rect rect;
+	bool to_delete = false;
+	COLLIDER_TYPE type;
+	j1Module* callback = nullptr;
+
+
+	Collider(SDL_Rect rectangle, COLLIDER_TYPE type, j1Module* callback = nullptr) :
+		rect(rectangle),
+		type(type),
+		callback(callback)
+	{}
+
+	void SetPos(int x, int y)
+	{
+		rect.x = x;
+		rect.y = y;
+	}
+
+	bool CheckCollision(const SDL_Rect& r) const;
+};
+
+class j1Collision : public j1Module
+{
+public:
+
+	j1Collision();
+	~j1Collision();
+
+	bool PreUpdate() override;
+	bool Update();
+	bool CleanUp() override;
+
+	Collider* AddCollider(SDL_Rect rect, COLLIDER_TYPE type, j1Module* callback = nullptr);
+	void DebugDraw();
+
+private:
+
+	Collider * colliders[MAX_COLLIDERS];
+	bool matrix[COLLIDER_MAX][COLLIDER_MAX];
+	bool debug = false;
+};
+
+#endif // __ModuleCollision_H__
---
+++ Sebi/Platform Game _ Sebi Coll/Motor2D/j1Player.cpp
@@ -0,0 +1,120 @@
+#include "j1App.h"
+#include "j1Textures.h"
+#include "j1Player.h"
+#include "j1Module.h"
+#include "j1Input.h"
+#include "j1Render.h"
+#include "j1Textures.h"
+#include "j1App.h"
+#include "p2Log.h"
+
+#define PLAYER_SPEED 1.f
+
+j1Player::j1Player()
+{
+	name.create("player");
+
+	idle.PushBack({ 14,7,19,29 });
+	idle.PushBack({ 66,6,17,30 });
+	idle.PushBack({ 115,6,19,30 });
+	idle.PushBack({ 163,7,20,29 });
+	idle.speed = 0.15f;
+	idle.loop = true;
+
+	run_forward.PushBack({ 67,45,20,28 });
+	run_forward.PushBack({ 116,46,20,27 });
+	run_forward.PushBack({ 166,48,20,25 });
+	run_forward.PushBack({ 217,45,23,28 });
+	run_forward.PushBack({ 266,46,20,27 });
+	run_forward.PushBack({ 316,48,20,25 });
+	run_forward.speed = 0.15f;
+	run_forward.loop = true;
+
+	jump.PushBack({ 317, 340, 23, 29 });
+	jump.PushBack({ 18, 377, 22, 29 });
+	jump.PushBack({ 68, 377, 22, 29 });
+	jump.speed = 0.15f;
+	jump.loop = false;
+
+}
+
+j1Player::~j1Player()
+{
+}
+
+bool j1Player::Awake(pugi::xml_node& node)
+{
+	return true;
+}
+
+bool j1Player::Start()
+{
+	bool ret = true;
+	player_texture = App->tex->Load("textures/character.png");
+	if (player_texture != nullptr)
+	{
+		LOG("Player Texture Loaded Succesfully");
+	}
+	else
+	{
+		LOG("There was an error loading Player texture.");
+	}
+
+
+	position.x = 10.f;
+	position.y = 300.f;
+	return true;
+}
+
+bool j1Player::PreUpdate()
+{
+
+
+
+	return true;
+}
+
+bool j1Player::Update(float dt)
+{
+	current_animation = &idle;
+
+
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		position.x += PLAYER_SPEED;
+		current_animation = &run_forward;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		position.x -= PLAYER_SPEED;
+		current_animation = &run_backward;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_REPEAT) {
+		position.y -= PLAYER_SPEED;
+		current_animation = &jump;
+	}
+	App->render->Blit(player_texture, position.x, position.y, &(current_animation->GetCurrentFrame()));
+
+	return true;
+}
+
+bool j1Player::CleanUp()
+{
+	App->tex->UnLoad(player_texture);
+	return true;
+}
+
+bool j1Player::BlitImage() {
+
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		App->render->Blit(player_texture, position.x, position.y, &(current_animation->GetCurrentFrame()), SDL_FLIP_HORIZONTAL);
+	}
+	else {
+		App->render->Blit(player_texture, position.x, position.y, &(current_animation->GetCurrentFrame()));
+	}
+	return true;
+}
+
+void j1Player::OnCollision(Collider* c1, Collider* c2){
+
+}
